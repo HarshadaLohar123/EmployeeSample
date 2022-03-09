3894c15f022522e2e3050268210104bcbacfe564
harshadalohar123 <harshalohar123@gmail.com>
2022-03-09 16:21:39 +0530

    Initial commit

---
+++ EmployeeWage/EmployeeWage/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Class1
+    {
+        public void Sample()
+        {
+            Console.WriteLine("Hello Akanksha");
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.Sample();
+        }
+    }
+}
